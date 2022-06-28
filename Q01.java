8952053d28e141e80d1a73e80ca9349a40d26555
zeecaixao22 <62753858+zeecaixao22@users.noreply.github.com>
2022-06-28 18:26:05 -0300

    Questão 1

---
+++ Q01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Q01 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int Nota1 = ler.nextInt();
+		System.out.println(Nota1);
+	}
+}
